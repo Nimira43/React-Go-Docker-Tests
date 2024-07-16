19557ba6461df4465fc0c7030d379127b3faa738
Nick Rabone <n.rabone72@aol.com>
2024-07-16 17:48:04 +0100

    Using functional components

--- src/AppClass.js
+++ src/AppClass.js
@@ -4,8 +4,8 @@ export default class AppClass extends Component {
   render() {
     return (
       <div>
-      <h1>React Go Docker Test</h1>
-      <p>Using Classes</p>
+        <h1>React Go Docker Tests</h1>
+        <p>Using Classes</p>
       </div>  
     )        
   }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import AppClass from './AppClass'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <AppClass />
+    <App />
   </React.StrictMode>
 )
 
