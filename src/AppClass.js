19b4c52a9596683d1b292ae54f41c011a9e0a375
Nick Rabone <n.rabone72@aol.com>
2024-07-16 17:23:37 +0100

    Using old  fashioned Classes

---
+++ src/AppClass.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class AppClass extends Component {
+  render() {
+    return (
+      <div>
+      <h1>React Go Docker Test</h1>
+      <p>Using Classes</p>
+      </div>  
+    )        
+  }
+}
